0585afd61fa2dfd24adcda78e52865cea6381aa7
FerdiLeCodeur <ferdi_Codeur@protonmail.com>
2022-03-18 13:38:33 +0100

    ajout pagination et correction du controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,15 +7,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(CallApiService $callApiService): Response
     {
-        //dd($callApiService->getFranceData());
-
+        $page = $callApiService->getFranceData();
         return $this->render('home/index.html.twig', [
-            'data'=> $callApiService->getFranceData(),
+            'data'=> $page,
+        ]);
+    }
+
+    //_______________________________________________________________________
+    #[Route('/page/{numPage}', name: 'pages')]
+    public function page(CallApiService $callApiService, int $numPage = 1): Response
+    {
+
+        $elementPage = 20;
+
+        if($numPage < 1){
+            $numPage = 1;
+        }
+
+        $data = $callApiService->getFranceData();
+
+        $currentPage = ($elementPage * ($numPage-1));
+        //dump($currentPage);
+        //dd($page[1]['title']);// titre du 1er tableau
+
+
+        $cutPage =array_chunk($data,20);
+        //dd($cutPage[$numPage-1]);
+        $pageDatas = $cutPage[$numPage-1];
+
+        //_________________return____________________________________________
+
+        return $this->render('pages/index.html.twig', [
+            'numPage'=> $numPage,
+            'pageDatas' => $pageDatas,
+
+
         ]);
     }
 }
